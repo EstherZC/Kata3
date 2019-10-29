b5bb2b3006335192389dd045f80dd3a386b7928c
Esther <>
2019-10-29 16:47:34 +0000

    Kata3 versión 1

---
+++ src/kata3/main.java
@@ -0,0 +1,13 @@
+package kata3;
+/**
+ *
+ * @author Esther
+ */
+public class main {
+
+    public static void main(String[] args) {
+        HistogramDisplay histo = new HistogramDisplay();
+        histo.execute();
+    }
+    
+}
